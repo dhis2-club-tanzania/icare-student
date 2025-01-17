2496c94e12a9625e1f871ca1ef701e552a913b74
MiniMsuya <halimamsuya477@gmail.com>
2025-01-17 21:12:54 +0300

    Halima Msuya 2022-04-08634 Creating functionality to clear all cookies when a user logs out using cookie.split() function and changed the previous OrderUuid

--- ui/src/app/core/components/menu/menu.component.ts
+++ ui/src/app/core/components/menu/menu.component.ts
@@ -152,6 +152,12 @@ export class MenuComponent implements OnInit {
     window.localStorage.clear();
     sessionStorage.clear();
     this.store.dispatch(logoutUser());
+    // functinality to clear all cookies when user logs out
+    document.cookie.split(";").forEach((cookie) => {
+      const eqPos = cookie.indexOf("=");
+      const name = eqPos > -1 ? cookie.substring(0,eqPos).trim() : cookie.trim();
+      document.cookie = '${name}=; expires = Thur, 01 Jan 2025 00:00:00 GMT;PATH=/';
+    })
   }
 
   onOpenLocation(e: Event): void {
