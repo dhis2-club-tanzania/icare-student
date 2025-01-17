efb43ac0a0fcc3c3592463b2e26ee14f684dbb00
Karenbhoke <masekekaren6@gmail.com>
2025-01-17 19:00:31 +0300

    We added methods and functions so as to display admission and discharge details in the history

--- ui/src/app/shared/components/patient-history/patient-history.component.spec.ts
+++ ui/src/app/shared/components/patient-history/patient-history.component.spec.ts
@@ -1,25 +1,284 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  Component,
+  EventEmitter,
+  Inject,
+  Input,
+  OnInit,
+  Output,
+  ViewChild,
+} from "@angular/core";
+import { Observable } from "rxjs";
+import { map, switchMap, tap } from "rxjs/operators";
+import { VisitsService } from "../../resources/visits/services/visits.service";
+import { Store } from "@ngrx/store";
+import { AppState } from "src/app/store/reducers";
+import {
+  getAdmissionStatusOfCurrentPatient,
+  getAdmittingLoadingState,
+} from "src/app/store/selectors/current-patient.selectors";
+import {
+  getAllForms,
+  getCustomOpenMRSFormById,
+  getCustomOpenMRSFormsByIds,
+} from "src/app/store/selectors/form.selectors";
+import { SystemSettingsService } from "src/app/core/services/system-settings.service";
+import { MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { getParentLocation } from "src/app/store/selectors";
+import {
+  getCurrentUserDetails,
+  getProviderDetails,
+} from "src/app/store/selectors/current-user.selectors";
+import {
+  PersonGetRef,
+  PrivilegeGetRef,
+  RoleGetRef,
+} from "../../resources/openmrs";
+import { loadCustomOpenMRSForm } from "src/app/store/actions";
+import { FormValue } from "../../modules/form/models/form-value.model";
+import { ICARE_CONFIG } from "../../resources/config";
+import { PatientHistoryDataComponent } from "../patient-history-data/patient-history-data.component";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { ObservationService } from "../../resources/observation/services";
 
-import { PatientHistoryComponent } from './patient-history.component';
+@Component({
+  selector: "app-patient-history",
+  templateUrl: "./patient-history.component.html",
+  styleUrls: ["./patient-history.component.scss"],
+})
+export class PatientHistoryComponent implements OnInit {
+  @Output() formDataSaved: EventEmitter<any> = new EventEmitter<any>();
+  // @Input() savedFormData: any;
+  form: FormGroup;
+  @Input() patient: any;
+  @Input() location: any;
+  saving: boolean = false;
 
-describe('PatientHistoryComponent', () => {
-  let component: PatientHistoryComponent;
-  let fixture: ComponentFixture<PatientHistoryComponent>;
+  formData: any;
+  visits$: Observable<any>;
+  add$:Observable<boolean>;
+  add1$:Observable<boolean>;
+  customForms$: Observable<any>;
+  generalPrescriptionOrderType$: any;
+  prescriptionArrangementFields$: Observable<any>;
+  specificDrugConceptUuid$: Observable<unknown>;
+  errors: any[] = [];
+  allForms$: Observable<any>;
+  loadingData: boolean = false;
+  facilityDetails$: Observable<any>;
+  currentUser$: Observable<{
+    userPrivileges: any;
+    links?: { rel?: string; uri?: string }[];
+    uuid?: string;
+    display?: string;
+    username?: string;
+    systemId?: string;
+    userProperties?: object;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ PatientHistoryComponent ]
-    })
-    .compileComponents();
-  });
+    // person?: PersonGetRef;
+    // privileges?: PrivilegeGetRef[];
+    // roles?: RoleGetRef[];
+    provider?: { uuid?: string; display?: string };
+  }>;
+  provider$: Observable<any>;
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(PatientHistoryComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  doctorsIPDRoundForm$: Observable<any>;
+  constructor(
+    private visitsService: VisitsService,
+    private store: Store<AppState>,
+    private systemSettingsService: SystemSettingsService,
+    private observationService: ObservationService
+  ) {}
+//data has to be initialized on start to work
+ // params: any;
+  get patientStatus(): boolean {
+    return this.add$?._isScalar;
+  }
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+
+  ngOnInit(): void {
+    this.loadData();
+
+    this.add$ = this.store.select(
+      getAdmissionStatusOfCurrentPatient
+    );
+  }
+
+
+  
+
+
+
+  private loadData(): void {
+    this.loadingData = true;
+    this.add1$=this.store.select(getAdmissionStatusOfCurrentPatient);
+    this.add$ = this.store.select( getAdmissionStatusOfCurrentPatient);
+    this.customForms$ = this.store.select(getAllForms);
+    this.facilityDetails$ = this.store.select(getParentLocation);
+    this.currentUser$ = this.store.select(getCurrentUserDetails);
+    this.provider$ = this.store.select(getProviderDetails);
+    this.generalPrescriptionOrderType$ =
+      this.systemSettingsService.getSystemSettingsByKey(
+        "iCare.clinic.genericPrescription.orderType"
+      );
+    this.prescriptionArrangementFields$ = this.systemSettingsService
+      .getSystemSettingsByKey("iCare.clinic.prescription.arrangement")
+      .pipe(
+        map((response) => {
+          if (response === "none") {
+            this.errors = [
+              ...this.errors,
+              {
+                error: {
+                  message:
+                    "Arrangement setting isn't defined, Set 'iCare.clinic.prescription.arrangement' or Contact IT (Close to continue)",
+                },
+                type: "warning",
+              },
+            ];
+          }
+          if (response?.error) {
+            this.errors = [...this.errors, response?.error];
+          }
+          return {
+            ...response,
+            keys: Object.keys(response).length,
+          };
+        })
+      );
+    this.specificDrugConceptUuid$ = this.systemSettingsService
+      .getSystemSettingsByKey(
+        "iCare.clinic.genericPrescription.specificDrugConceptUuid"
+      )
+      .pipe(
+        tap((response) => {
+          if (response === "none") {
+            this.errors = [
+              ...this.errors,
+              {
+                error: {
+                  message:
+                    "Generic Use Specific drug Concept config is missing, Set 'iCare.clinic.genericPrescription.specificDrugConceptUuid' or Contact IT (Close to continue)",
+                },
+                type: "warning",
+              },
+            ];
+          }
+        })
+      );
+    this.visits$ = this.visitsService
+      .getAllPatientVisits(this.patient?.uuid, true)
+      .pipe(
+        map((response) => {
+          this.loadingData = false;
+
+
+
+
+          
+          if (!response?.error) {
+            return response?.map((visit) => {
+              let obs = [];
+              visit?.visit?.encounters.forEach((encounter) => {
+                (encounter?.obs || []).forEach((observation) => {
+                  obs = [
+                    ...obs,
+                    {
+                      ...observation,
+                      encounterType: {
+                        uuid: encounter?.encounterType?.uuid,
+                        display: encounter?.encounterType?.display,
+                      },
+                      provider: encounter?.encounterProviders[0]?.provider,
+                    },
+                  ];
+                });
+              });
+              return {
+                visit: visit?.visit,
+                obs: obs,
+                orders: [
+                  ...visit?.visit?.encounters.map((encounter) => {
+                    return (encounter?.orders || []).map((order) => {
+                      return {
+                        ...order,
+                        encounterType: {
+                          uuid: encounter?.encounterType?.uuid,
+                          display: encounter?.encounterType?.display,
+                        },
+                      };
+                    });
+                  }),
+                ].filter((order) => order.length),
+              };
+            });
+          }
+        })
+      );
+
+    // this.getIPDRoundDoctorsForm();
+  }
+
+  // getIPDRoundDoctorsForm(): void {
+  //   this.systemSettingsService
+  //     .getSystemSettingsByKey("iCare.forms.doctorsIPDRound.uuid")
+  //     .subscribe((doctorsIPDRoundFormUuid: string) => {
+  //       if (doctorsIPDRoundFormUuid) {
+  //         this.store.dispatch(
+  //           loadCustomOpenMRSForm({ formUuid: doctorsIPDRoundFormUuid })
+  //         );
+  //         this.doctorsIPDRoundForm$ = this.store.select(
+  //           getCustomOpenMRSFormById(doctorsIPDRoundFormUuid)
+  //         );
+  //       }
+  //     });
+  // }
+
+
+ // params: any; // Replace with actual type
+ // loadingData: boolean = false;
+
+  
+
+
+
+  onDoctorsIPDRoundCommentsFormUpdate(formValue: FormValue): void {
+    console.log(formValue.getValues());
+    this.loadData();
+    this.formData = formValue.getValues();
+  }
+
+  onSave(event: Event, form: any, provider: any, visit: any): void {
+    event.stopPropagation();
+    this.loadData();
+    const obs = Object.keys(this.formData).map((key: string) => {
+      return {
+        concept: key,
+        value: this.formData[key]?.value,
+      };
+    });
+    let encounterObject = {
+      patient: this.patient?.uuid,
+      encounterType: form?.encounterType?.uuid,
+      location: this.location?.uuid,
+      encounterProviders: [
+        {
+          provider: provider?.uuid,
+          encounterRole: ICARE_CONFIG?.encounterRole?.uuid,
+        },
+      ],
+      visit: visit?.uuid,
+      obs: obs,
+      form: {
+        uuid: form?.uuid,
+      },
+    };
+
+    this.observationService
+      .saveEncounterWithObsDetails(encounterObject)
+      .subscribe((res) => {
+        if (res) {
+          this.loadData();
+        }
+      });
+  }
+}
