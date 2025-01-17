b2fa12f46bfdb6556dda7b1767427f27337a2fcf
Sarapion13 <sarapionstephano@gmail.com>
2025-01-18 00:16:39 +0300

    first

---
+++ ui/src/app/modules/pharmacy/modules/dashboard/dashboard.module.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { SharedModule } from "src/app/shared/shared.module";
+import { pages } from "./pages";
+import { components } from "./components";
+import { DashboardRoutingModule } from "./dashboard-routing.module";
+
+@NgModule({
+    declarations: [...pages, ...components],
+    imports: [CommonModule, DashboardRoutingModule, SharedModule]
+})
+export class DashboardModule {}
