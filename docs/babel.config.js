18067ac388082b4c62b470cba349d95c25212712
rahel21charles <162223938+rahel21charles@users.noreply.github.com>
2025-01-19 22:17:39 +0300

    Registered the added effect

--- docs/babel.config.js
+++ docs/babel.config.js
@@ -1,3 +1,4 @@
-module.exports = {
-  presets: [require.resolve('@docusaurus/core/lib/babel/preset')],
+module.exports = {
+  presets: [require.resolve('@docusaurus/core/lib/babel/preset')],
 };
+
